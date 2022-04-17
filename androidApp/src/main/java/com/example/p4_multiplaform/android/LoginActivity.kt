ce5cbe96d2f9ce8ae157c6f0f42617799cdfc340
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-04-17 00:11:52 -0400

    Finish `LoginActivity.kt` & make circle drawable for player piece

--- androidApp/src/main/java/com/example/p4_multiplaform/android/LoginActivity.kt
+++ androidApp/src/main/java/com/example/p4_multiplaform/android/LoginActivity.kt
@@ -2,48 +2,47 @@ package com.example.p4_multiplaform.android
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-
 class LoginActivity : AppCompatActivity() {
-
-    private fun login(player_1: String, player_2: String) {
-        val intent = Intent(this, GameActivity::class.java)
-        intent.putExtra("player_1", player_1)
-        intent.putExtra("player_2", player_2)
-        startActivity(intent)
-        // save player names in shared preferences
-        val sharedPref = getSharedPreferences("player_names", 0)
-        val editor = sharedPref.edit()
-        editor.putString("player_1", player_1)
-        editor.putString("player_2", player_2)
-        editor.apply()
-    }
-
-    private fun checkInputBeforeButtonClick(username: String, password: String) {
-        if (username.isEmpty() || password.isEmpty()) {
-            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
-        } else {
-            login(username, password)
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Get the players names
         val player1 = findViewById<android.widget.EditText>(R.id.player_1_name)
         val player2 = findViewById<android.widget.EditText>(R.id.player_2_name)
-        val loginButton = findViewById<android.widget.Button>(R.id.startButton)
 
-        loginButton.setOnClickListener {
-            checkInputBeforeButtonClick(player1.text.toString(), player2.text.toString())
+        //  Get the start Button
+        val btn = findViewById<android.widget.Button>(R.id.startButton)
+
+        // Set the button on click listener
+        btn.setOnClickListener {
+            // Get the players names
+            val namePlayer1 = player1.text.toString()
+            val namePlayer2 = player2.text.toString()
+
+            if(namePlayer1.isEmpty() || namePlayer2.isEmpty()){
+                // Create a toast message
+                val toast = android.widget.Toast.makeText(this, "Please enter your nicknames", android.widget.Toast.LENGTH_SHORT)
+                // Show the toast message
+                toast.show()
+
+            }else{
+
+                // Create the intent
+                val intent = Intent(this, GameActivity::class.java)
+
+                // Put the players names in the intent
+                intent.putExtra("namePlayer1", namePlayer1)
+                intent.putExtra("namePlayer2", namePlayer2)
+
+                // Start the activity
+                startActivity(intent)
+
+                // Finish this activity
+                finish()
+            }
         }
-
-
     }
-
-
-
 }
